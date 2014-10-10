a53c1b0f17d42d7d7b1b6b331cbccc6022e2f3de
Andrew Wallace <andrew.g.wallace@gmail.com>
2014-10-10 19:26:50 -0400

    add helpers and test cases for page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,23 +6,22 @@ class StaticPagesControllerTest < ActionController::TestCase
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
+test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
@@ -31,3 +30,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Contact | #{@base_title}"
   end
 end
+
